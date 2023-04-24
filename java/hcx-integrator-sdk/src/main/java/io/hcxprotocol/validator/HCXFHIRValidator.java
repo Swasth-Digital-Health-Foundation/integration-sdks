31ae3e4380c8a71a4babd54bf634c4db9c471cb2
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-04-24 17:15:19 +0530

    Update HCXFHIRValidator.java

--- java/hcx-integrator-sdk/src/main/java/io/hcxprotocol/validator/HCXFHIRValidator.java
+++ java/hcx-integrator-sdk/src/main/java/io/hcxprotocol/validator/HCXFHIRValidator.java
@@ -108,7 +108,7 @@ public class HCXFHIRValidator {
     }
 
     public void loadProfiles(PrePopulatedValidationSupport prePopulatedSupport, IParser parser, String type,FhirContext fhirContext) throws FileNotFoundException {
-        File dir = new File(System.getProperty("user.dir") + "\\" + type);
+        File dir = new File(System.getProperty("user.dir") + type);
         File[] directoryList = dir.listFiles();
         if (directoryList != null) {
             for (File file : directoryList) {
@@ -136,7 +136,7 @@ public class HCXFHIRValidator {
     }
 
     public void downloadAndExtractZip(String type , String url, String zipFileName) throws IOException {
-        String currentDir = System.getProperty("user.dir") + "\\";
+        String currentDir = System.getProperty("user.dir") ;
         Path newDir = Paths.get(currentDir, type);
         downloadZip(new URL(url),currentDir + zipFileName);
         if(isPresent(newDir)){
